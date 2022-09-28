14296422a849b0580015fa22e3ef4de8a084a0b0
Lasma Poksane <laasmapoksane@gmail.com>
2022-09-28 12:49:16 +0200

    add handleNameChange method to Playlist

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,10 +3,19 @@ import TrackList from "../TrackList/TrackList";
 import React from "react";
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={"New Playlist"} />
+        <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
         <TrackList
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
