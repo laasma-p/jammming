80f583f83c78324da2e33e6e118f565bccc31ae7
Lasma Poksane <laasmapoksane@gmail.com>
2022-09-29 12:41:08 +0200

    implement search functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -57,6 +57,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -90,6 +91,10 @@ class App extends React.Component {
     });
   }
 
+  search(searchTerm) {
+    console.log(searchTerm);
+  }
+
   render() {
     return (
       <div>
@@ -97,7 +102,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
