60fdf646d3e22c20d35ee64c1b472f3cd9e535a5
Lasma Poksane <laasmapoksane@gmail.com>
2022-10-02 00:27:47 +0200

    obtain Spotify Access Token

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+const clientID = "Your client ID goes here";
+const redirectURI = "Your redirect URI goes here";
+let userAccessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (userAccessToken) {
+      return userAccessToken;
+    }
+
+    // Check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      userAccessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      // This clears the parameters, allowing to grab a new access token when it expires
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+    } else {
+      const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessURL;
+    }
+  },
+};
+
+export default Spotify;
